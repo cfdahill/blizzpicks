b739fbab0ae056abab7e7e5ec9c2572e0842f81b
cfdahill <cfdahill@gmail.com>
2018-10-31 22:16:59 -0700

    mongodb

--- models/mongo.js
+++ models/mongo.js
@@ -8,10 +8,10 @@ const MONGO_LOCAL_URL = "mongodb://localhost:27017/esportdb"
 
 // if (process.env.MONGODB_URI) {
   console.log(process.env.MONGODB_URI);
-	mongoose.connect(process.env.MONGODB_URI)
-	MONGO_URL = process.env.MONGODB_URI
+	mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
+	MONGO_URL = process.env.MONGODB_URI;
 // } else {
-// 	mongoose.connect(MONGO_LOCAL_URL, {useNewUrlParser: true}) // local mongo url
+	// mongoose.connect(MONGO_LOCAL_URL, {useNewUrlParser: true}) // local mongo url
 // 	MONGO_URL = MONGO_LOCAL_URL
 // }
 
